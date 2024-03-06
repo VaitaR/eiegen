b5c1e7fb3eda5182348786eb724e618ebcb0860c
VaitaR <andrey.shivalin@phystech.edu>
2024-03-06 10:58:50 +0300

    main page fix

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ st.title('Welcome to Monitoring dash for InceptionLRT! 👋 ')
 st.write('This is a simple dashboard to monitor the InceptionLRT smart contract')
 # information and links on pages
 st.write('The dashboard has the following pages:')
-st.write('1. [Withdrawals_dash](http://https://inception-monitor.streamlit.app/?page=withdrawals_dash)')
-st.write('2. [Assets VS Withdraw](http://https://inception-monitor.streamlit.app/?page=Assets_Vs_Withdraw)')
+st.write('1. [Withdrawals_dash](https://inception-monitor.streamlit.app/withdrawals_dash)')
+st.write('2. [Assets VS Withdraw](https://inception-monitor.streamlit.app/Assets_Vs_Withdraw)')
