78c46c6d89f36eb14078967c4ef1cdf2adfff444
VaitaR <andrey.shivalin@phystech.edu>
2024-03-06 14:09:20 +0300

    cashe fix

--- pages/1 Assets_Vs_Withdraw.py
+++ pages/1 Assets_Vs_Withdraw.py
@@ -8,25 +8,24 @@ from dotenv import load_dotenv
 load_dotenv()
 curr_dir = os.path.dirname(os.path.dirname(__file__))
 
-@st.cache(ttl=3000)
+@st.cache_data(ttl=1800)
 def get_block():
     current_block = int(w3.eth.get_block('latest')['number'])
-    st.write(f"Current block: {current_block}")
     return current_block
 
-@st.cache(ttl=3000)
+@st.cache_data(ttl=1800)
 def get_proxy_abi(address:str):
     proxy_contract = json.loads(requests.get('https://api.etherscan.io/api?module=contract&action=getsourcecode&address=' + address + '&apikey=' + os.getenv('ETHERSCAN_KEY')).text)['result']
     proxy_contract_address = proxy_contract[0]['Implementation']
     proxy_contract_abi = json.loads(requests.get('https://api.etherscan.io/api?module=contract&action=getabi&address=' + proxy_contract_address + '&apikey=' + os.getenv('ETHERSCAN_KEY')).text)['result']
     return proxy_contract_abi
 
-@st.cache(ttl=3000)
+@st.cache_data(ttl=1800)
 def load_wallets():
     wallets = json.load(open(os.path.join(curr_dir, 'inception_wallets.json')))['wallets']
     return wallets
 
-@st.cache(ttl=3000)
+@st.cache_data(ttl=1800)
 def wallet_rpc_stats(wallet, current_block:int):
     address = wallet['address']
     name = wallet['name']
@@ -64,7 +63,10 @@ def wallet_rpc_stats(wallet, current_block:int):
 
 
 w3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth/' + os.getenv('ANKR_KEY')))
+
 current_block = get_block()
+st.write(f"Current block: {current_block}")
+
 wallets = load_wallets()
 
 for wallet in wallets:
